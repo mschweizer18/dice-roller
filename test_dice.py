0fce7d1237efb8f06a691b7ccef81b1e4500857c
Maya Schweizer <maya.schweizer18@gmail.com>
2024-09-30 14:13:26 +0000

    Fix API always returning 0 with TDD

---
+++ test_dice.py
@@ -0,0 +1,9 @@
+from dice import Dice
+
+class TestDice:
+    def test_one_roll(self):
+        dice = Dice()
+        roll = dice.roll()
+        assert type(roll) == int
+        assert roll > 0
+        assert roll < 7
